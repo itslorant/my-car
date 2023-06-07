deef9943874fe03d00dbee49b4b71f02e5ed9c08
itslorant <fihumoto@gmail.com>
2023-06-07 12:20:01 +0200

    feat: add car screen and placeholders

---
+++ lib/screens/cars.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class Cars extends StatelessWidget {
+  const Cars({super.key});
+
+  final carList = const [
+    {'id': 1, 'brand': 'Opel', 'type': 'Astra', 'licencePlateNr': 'JEF-399'},
+    {'id': 2, 'brand': 'Skoda', 'type': 'Octavia', 'licencePlateNr': 'RHK-901'},
+    {'id': 3, 'brand': 'VW', 'type': 'Touran', 'licencePlateNr': 'OFOW72'}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('My cars')),
+        body: ListView.builder(
+          itemCount: carList.length,
+          itemBuilder: (context, index) => Card(
+            color: Colors.green[50],
+            child: Column(
+              children: [
+                Image.asset('assets/images/vintage-car.png', height: 150),
+                Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                      gradient:
+                          LinearGradient(colors: [Colors.white, Colors.green])),
+                  width: double.infinity,
+                  height: 30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                          '${carList[index]['brand']} ${carList[index]['type']}'),
+                      Text('${carList[index]['licencePlateNr']}'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
